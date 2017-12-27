7a4e1630b3a0c12695932d6bacc819c31cf7209b
artem.gazizyanov <artem.gazizyanov@travelline.ru>
2017-12-27 15:07:16 +0300

    табы - пробелы

--- src/Lexer/Token.cs
+++ src/Lexer/Token.cs
@@ -1,26 +1,26 @@
 ﻿namespace Compiler.LexicalAnalyzer
 {
-	public sealed class Token
-	{
-		public Token(TokenType type, int start, int row, string text = "")
-		{
-			Type = type;
-			Start = start;
-			Row = row;
-			Text = text;
-		}
+    public sealed class Token
+    {
+        public Token(TokenType type, int start, int row, string text = "")
+        {
+            Type = type;
+            Start = start;
+            Row = row;
+            Text = text;
+        }
 
-		public TokenType Type { get; }
-		public string Text { get; }
-		public int Start { get; }
-		public int End => Start + (Text.Length > 0 ? Text.Length - 1 : 0);
-	    public int Row { get; }
+        public TokenType Type { get; }
+        public string Text { get; }
+        public int Start { get; }
+        public int End => Start + (Text.Length > 0 ? Text.Length - 1 : 0);
+        public int Row { get; }
 
-		public override string ToString()
-		{
-			return string.Format(PATTERN, Text, Type, Start, End, Row);
-		}
+        public override string ToString()
+        {
+            return string.Format(PATTERN, Text, Type, Start, End, Row);
+        }
 
-		private readonly string PATTERN = "Token:  Text=\"{0}\" Type=\"{1}\" First=\"{2}\" Last=\"{3}\" Row = \"{4}\"";
-	}
+        private readonly string PATTERN = "Token:  Text=\"{0}\" Type=\"{1}\" First=\"{2}\" Last=\"{3}\" Row = \"{4}\"";
+    }
 }
